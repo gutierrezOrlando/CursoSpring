cc5beea31ade703b0542570c75237379b66e5d53
Orlando Gutierrez <orlando.gutierrez@readinessit.com>
2020-06-24 19:00:53 -0400

    primer commit de fuentes curso spring

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES(1,'Andres','Guzman','profesor@gmail.com','2017-08-28');
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES(2,'Orlando','Gutierrez','test@gmail.com','2020-12-05');
